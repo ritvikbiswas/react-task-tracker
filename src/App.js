f02f3d73776455acbc420eb1a33590de4ecc1f78
Ritvik Biswas <ritvik.biswas@gmail.com>
2021-06-21 01:10:42 -0400

    toggle reminder func added

--- src/App.js
+++ src/App.js
@@ -10,32 +10,41 @@ function App() {
             id:1,
             text: "Doctor's Appointment",
             day: "Feb 5th at 2:30pm",
-            reminder: "true"
+            reminder: true
         },
         {
             id:2,
             text: "Meeting at School",
             day: "Feb 6th at 1:30pm",
-            reminder: "true"
+            reminder: true
         },
         {
             id:3,
             text: "Food Shopping",
             day: "Feb 5th at 2:30pm",
-            reminder: "false"
+            reminder: false
         }    
     ]
   );
 
   //delete task
-    const deleteTask = (id) => {
-      setTasks(tasks.filter((task) => task.id !== id));
-    }
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) => 
+        task.id === id ? {...task, reminder: !task.reminder} : task
+      )
+    )
+  } 
 
   return (
     <div className="container">
       <Header title='Task Tracker'/>
-      {tasks.length > 0 ? <Tasks tasks= {tasks} onDelete={deleteTask} /> : 'No tasks to show! Add a task!'}
+      {tasks.length > 0 ? <Tasks tasks= {tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No tasks to show! Add a task!'}
     </div>
   );
 }
