d32467a6d46db43bd24935b1dbffd9a095eb10c0
Ritvik Biswas <ritvik.biswas@gmail.com>
2021-06-20 22:02:32 -0400

    need to add delete functionality to the task component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,35 @@
+//import React from 'react' -> don't need anymore
 import PropTypes from 'prop-types';
+import Button from './Button'
+
 
 const Header = (props) => {
+    const AddTaskButtonOnClick = (e) => {
+        console.log('click');
+    } 
+
     return (
-        <div>
+        <header className='header'>
             <h1>{props.title}</h1>
-        </div>
+            <Button color='green' text ='Add Task' onClick = {AddTaskButtonOnClick}/>
+        </header>
+        
     )
 }
 
-Header.defaultProps = {
-    title: 'Task Tracker'
-};
+Header.defaultProps ={
+    title: 'Add to Title to Your Header!'
+}
 
-Header.propTypes = {
+Header.propTypes ={
     title: PropTypes.string
 }
+
+/**
+const headingStyle = {
+    color: 'red', 
+    backgroundColor:'black'
+}
+*/
 
 export default Header
---
+++ src/components/Tasks.js
@@ -0,0 +1,13 @@
+import Task from './Task'
+
+const Tasks = ({tasks}) => {
+    return (
+        <div>
+            {tasks.map((task) => (
+                <Task task = {task}/>)
+                )}
+        </div>
+    )
+}
+
+export default Tasks
